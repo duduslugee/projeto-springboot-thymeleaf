892e7a17cbb0cdc80f89ed3f6f1479298d0a567b
Eduardo Ramos <edupowergames2@gmail.com>
2024-09-23 23:30:25 -0300

    test

--- src/main/java/br/duduslugee/projeto/repository/ContasDoMesRepository.java
+++ src/main/java/br/duduslugee/projeto/repository/ContasDoMesRepository.java
@@ -1,2 +1,9 @@
-package br.duduslugee.projeto.repository;public interface ContasDoMesRepository {
+package br.duduslugee.projeto.repository;
+
+import br.duduslugee.projeto.model.ContasDoMes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContasDoMesRepository extends JpaRepository<ContasDoMes, Long> {
 }
